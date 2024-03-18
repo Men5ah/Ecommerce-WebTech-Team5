ce938f5c449e1ed12d8867a7f065b92952b8ca3a
Men5ah <96393336+Men5ah@users.noreply.github.com>
2024-03-18 15:18:04 +0000

    ggg

--- settings/connection.php
+++ settings/connection.php
@@ -3,8 +3,9 @@ $servername = "localhost";
 $username = "root";
 $password =  "cs341webtech";
 $database = "KRMBJ";
+$port = 3306;
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
