ed06de105528431091e168373a752ed89d05382d
Men5ah <96393336+Men5ah@users.noreply.github.com>
2024-03-22 10:10:59 +0000

    Deleted everything from main

--- README.md
+++
@@ -1,2 +0,0 @@
-# Ecommerce-WebTech-Team5
-This is the website for our team project. Other details will be added soon.
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Location: ../Ecommerce-WebTech-Team5/FrontendEcomXpress/views/login.php');
-?>
-
-<!-- http://localhost/Web Technologies Team Folder/GitHub/Ecommerce-WebTech-Team5/index.php -->
