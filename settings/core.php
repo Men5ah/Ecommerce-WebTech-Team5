285c563f9058823658645649b9949d53da1ab535
Men5ah <96393336+Men5ah@users.noreply.github.com>
2024-03-22 06:04:35 +0000

    Trying to fic user homepage

--- settings/core.php
+++ settings/core.php
@@ -3,8 +3,9 @@ session_start();
 
 function checkLogin()
 {
-    if (!($_SESSION['user_id'])) {
+    if (!isset($_SESSION['person_id'])) {
         header("Location: ../login/login_view.php");
         die();
     }
 }
+?>
